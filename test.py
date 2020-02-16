5db834938d5a192fd3d255e2a3e2e2e840beb968
macvincent <macvincentaghaoko@gmail.com>
2020-02-16 15:11:55 -0500

    feat: Created recap file

--- test.py
+++ test.py
@@ -89,12 +89,6 @@ def find_lane_pixels(binary_warped):
         win_xright_low = rightx_current - margin
         win_xright_high = rightx_current + margin
         
-#         # Draw the windows on the visualization image
-#         cv2.rectangle(out_img,(win_xleft_low,win_y_low),
-#         (win_xleft_high,win_y_high),(0,255,0), 2) 
-#         cv2.rectangle(out_img,(win_xright_low,win_y_low),
-#         (win_xright_high,win_y_high),(0,255,0), 2) 
-        
         ### TO-DO: Identify the nonzero pixels in x and y within the window ###
 
         nonzero = binary_warped.nonzero()
@@ -131,45 +125,13 @@ def find_lane_pixels(binary_warped):
 
     return leftx, lefty, rightx, righty, out_img
 
-
-def fit_polynomial(binary_warped):
-    # Find our lane pixels first
-    leftx, lefty, rightx, righty, out_img = find_lane_pixels(binary_warped)
-
-    ### TO-DO: Fit a second order polynomial to each using `np.polyfit` ###
-    left_fit = np.polyfit(lefty, leftx, 2)
-    right_fit = np.polyfit(righty, rightx, 2)
-#     print(left_fit)
-#     print(right_fit)
-    # Generate x and y values for plotting
-    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
-    try:
-        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
-        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
-    except TypeError:
-        # Avoids an error if `left` and `right_fit` are still none or incorrect
-        print('The function failed to fit a line!')
-        left_fitx = 1*ploty**2 + 1*ploty
-        right_fitx = 1*ploty**2 + 1*ploty
-
-    ## Visualization ##
-    # Colors in the left and right lane regions
-    out_img[lefty, leftx] = [255, 0, 0]
-    out_img[righty, rightx] = [0, 0, 255]
-
-    # Plots the left and right polynomials on the lane lines
-#     plt.plot(left_fitx, ploty, color='yellow')
-#     plt.plot(right_fitx, ploty, color='yellow')
-
-    return out_img, left_fitx, right_fitx
-
 def warp(img):
     img_size = (img.shape[1], img.shape[0])
     src = np.float32(
         [[275, 699],
         [1085, 681],
-        [741, 466],
-        [572, 461],        
+        [769, 487],
+        [546, 487],        
         ])
     dst = np.float32([
         [285, 691],
@@ -199,23 +161,27 @@ def unwarp(warped, dst, src, left_fitx, right_fitx):
     newwarp = cv2.warpPerspective(color_warp, Minv, (warped.shape[1], warped.shape[0])) 
     return newwarp
 
-# Load our image
-def find_lane(image):
-    if(image.shape != (720, 1280, 3)):
-        image = cv2.resize(image, (1280, 720), interpolation = cv2.INTER_AREA)
-    undistorted_image = distortion_correction(image)
-
-#     sobel_x = abs_sobel_thresh(undistorted_image)
-    sobel_x = mag_thresh(image, sobel_kernel=3, mag_thresh=(30, 100))
-    s_segment = hls_select(undistorted_image)
-    final_combination = np.zeros_like(s_segment)
-    final_combination[(s_segment == 1) | (sobel_x == 1)] = 1
-    warped, dst, src = warp(final_combination)
-    fitted, left_fitx, right_fitx = fit_polynomial(warped)
-
-    unwarped = unwarp(final_combination, dst, src, left_fitx, right_fitx)
-    result = cv2.addWeighted(image, 1, unwarped, 0.3, 0)
-    return result
+def measure_curvature(binary_warped, left_fit, right_fit):
+    '''
+    Calculates the curvature of polynomial functions in meters.
+    '''
+    # Define conversions in x and y from pixels space to meters
+    ym_per_pix = 30/720 # meters per pixel in y dimension
+    xm_per_pix = 3.7/700 # meters per pixel in x dimension
+    
+    # Start by generating our fake example data
+    # Make sure to feed in your real data instead in your project!
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+    
+    # Define y-value where we want radius of curvature
+    # We'll choose the maximum y-value, corresponding to the bottom of the image
+    y_eval = np.max(ploty)*ym_per_pix
+    
+    ##### TO-DO: Implement the calculation of R_curve (radius of curvature) #####
+    left_curverad =  np.power((1+np.power((2*left_fit[0]*y_eval + left_fit[1]),2)),1.5)/np.abs(2*left_fit[0])## Implement the calculation of the left line here
+    right_curverad = np.power((1+np.power((2*right_fit[0]*y_eval + right_fit[1]),2)),1.5)/np.abs(2*right_fit[0])  ## Implement the calculation of the ri  ght line here
+    
+    return np.int((left_curverad+right_curverad)/2)
 
 def mag_thresh(img, mag_thresh, sobel_kernel):
     thresh_min, thresh_max, = mag_thresh
@@ -230,9 +196,58 @@ def mag_thresh(img, mag_thresh, sobel_kernel):
     sxbinary[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1 # we filter out parts of the image not within the threshold range
     return sxbinary
 
-image = cv2.imread('Screenshot.png')
-image = cv2.resize(image, (1280, 720), interpolation = cv2.INTER_AREA)
-result = find_lane(image)
-plt.imshow(result)
-plt.show()
+left_fit = np.array([ 1.22120921e-04, -1.49160585e-01,  3.29075123e+02])
+right_fit = np.array([2.90910728e-04, -2.05179515e-01,  1.08280543e+03])
 
+def fit_polynomial(binary_warped):
+    global left_fit, right_fit
+    # Find our lane pixels first
+    leftx, lefty, rightx, righty, out_img = find_lane_pixels(binary_warped)
+
+    ### TO-DO: Fit a second order polynomial to each using `np.polyfit` ###
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+    try:
+        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+    except TypeError:
+        # Avoids an error if `left` and `right_fit` are still none or incorrect
+        print('The function failed to fit a line!')
+        left_fitx = 1*ploty**2 + 1*ploty
+        right_fitx = 1*ploty**2 + 1*ploty
+
+    ## Visualization ##
+    # Colors in the left and right lane regions
+    out_img[lefty, leftx] = [255, 0, 0]
+    out_img[righty, rightx] = [0, 0, 255]
+
+    return left_fitx, right_fitx
+
+# Load our image
+def find_lane(image):
+    if(image.shape != (720, 1280, 3)):
+        image = cv2.resize(image, (1280, 720), interpolation = cv2.INTER_AREA)
+    undistorted_image = distortion_correction(image)
+
+    sobel_x = mag_thresh(image, sobel_kernel=3, mag_thresh=(30, 100))
+    s_segment = hls_select(undistorted_image)
+    final_combination = np.zeros_like(s_segment)
+    final_combination[(s_segment == 1) | (sobel_x == 1)] = 1
+    warped, dst, src = warp(final_combination)
+    
+    left_fitx, right_fitx = fit_polynomial(warped)
+
+    unwarped = unwarp(final_combination, dst, src, left_fitx, right_fitx)
+    result = cv2.addWeighted(image, 1, unwarped, 0.3, 0)
+    cv2.putText(result, 'Radius of curvature = {}m'.format(measure_curvature(binary_warped, left_fit, right_fit)), (20,200), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2)
+    return result
+
+imgs = glob.glob('./test_images/*.jpg')
+for i, img in enumerate(imgs):
+    image = cv2.imread(img)
+    result = find_lane(image)
+    plt.imshow(result)
+    cv2.imwrite('output_images/test'+str(i)+'.jpg',result)
+    plt.show()
